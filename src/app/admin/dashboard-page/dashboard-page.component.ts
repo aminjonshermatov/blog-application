c97ba7d20e97d361bc86a994a4ceb71a2c3caf79
aminjonshermatov <aminjonshermatov@gmail.com>
2021-03-04 11:24:46 +0500

    notification

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -3,6 +3,7 @@ import {AuthService} from '../shared/services/auth.service';
 import {PostsService} from '../../shared/posts.service';
 import {Post} from '../../shared/interfaces';
 import {Subscription} from 'rxjs';
+import {AlertService} from '../shared/services/alert.service';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -16,7 +17,8 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   searchStr = '';
 
   constructor(
-    private postsService: PostsService
+    private postsService: PostsService,
+    private alertService: AlertService
   ) {}
 
   ngOnInit(): void {
@@ -38,6 +40,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   remove(id: string): void {
     this.dSub = this.postsService.remove(id).subscribe(() => {
       this.posts = this.posts.filter(post => post.id !== id);
+      this.alertService.danger('Post was deleted');
     });
   }
 }
